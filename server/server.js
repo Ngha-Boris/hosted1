ed78d46d889b5ddacf6ac279f7cb0917f675f661
Boris <borisngha6@gmail.com>
2024-11-19 14:42:05 +0100

    chores: changes username

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const stripeClient = stripe(process.env.STRIPE_SECRET_KEY, {
 
 // Middleware
 app.use(cors({
-  origin: "https://valsuh45.github.io", // Replace with your GitHub Pages URL
+  origin: "https://Valsuh45.github.io",
 }));
 app.use(express.json());
 
